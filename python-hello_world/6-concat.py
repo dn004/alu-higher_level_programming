4847bcf7d161f95859999fbfacc40baa444bdf4c
David Ntwari <d.ntwari@alustudent.com>
2022-07-01 04:33:39 -0700

    x

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+
