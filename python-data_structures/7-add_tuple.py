1c60dab52065a55923d9368d127d7fa8b5c2aca4
David Ntwari <d.ntwari@alustudent.com>
2022-07-15 02:09:41 -0700

    darondo

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,7 +7,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     sum2 = (tuple_a[1] if ade > 1 else 0) + (tuple_b[1] if bisi > 1 else 0)
 
     return (sum1, sum2)
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
