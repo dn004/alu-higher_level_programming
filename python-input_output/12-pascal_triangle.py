56aad113e9292431b5b8624da6d4cc8563cbdb30
David Ntwari <d.ntwari@alustudent.com>
2022-07-31 04:39:46 -0700

    where's the party?

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-task12
+Module 12-pascal_triangle.py
+function that returns a list of lists of integers
+representing the Pascal’s triangle of n
 """
 
 
 def pascal_triangle(n):
     """
-    returns a list of lists of integers
+    returns a list of lists of integers representing
+    the Pascal’s triangle of n
     """
+
     if n <= 0:
         return []
-    res = []
-    l = []
-    for x in range(n):
-        row = []
-        for y in range(x + 1):
-            if x == 0 or y == 0 or x == y:
-                row.append(1)
-            else:
-                row.append(l[y] + l[y - 1])
-        l = row
-        res.append(row)
+    if n == 1:
+        return [[1]]
+
+    tri = [[1]]
+    for row in range(n-1):
+        tri.append([i+j for i, j in
+                   zip([0]+tri[-1], tri[-1] + [0])])
     return 
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """
-adds all arguments to a Python list, and then save them to a file
+Module 7-add_item.py
+script adds all arguments to a Python list, and save them to a file
 """
-import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-open("add_item.json", "a")
+f_name = "add_item.json"
+
 try:
-    l = load_from_json_file("add_item.json")
-except ValueError:
-    l = []
-save_to_json_file(l + sys.argv[1:], "add_item.json")
+    data = load_from_json_file(f_name)
+except FileNotFoundError:
+    data = []
+
+save_to_json_file(data + argv[1:], f_name)
