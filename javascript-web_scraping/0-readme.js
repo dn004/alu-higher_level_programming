6954687a5779c8a3dea2ccb353e4d55456ab3f51
dn004 <d.ntwari@alustudent.com>
2022-10-14 01:31:28 -0700

    jinsei ooo

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -5,10 +5,9 @@ const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', (error, data) => {
-  if (error){
-	  console.error(error)
-  }
-  else {
+  if (error) {
+	  console.error(error);
+  } else {
 	  console.log(data);
   }
 });
